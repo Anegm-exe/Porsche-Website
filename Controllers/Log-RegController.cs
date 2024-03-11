7f4b341c6667843c22aeedcf91dbe62dadff19f2
Ahmed Essam Ahmed Hassan <ahmed.essam@student.giu-uni.de>
2024-03-11 20:18:21 +0200

    Log/Reg Page Update

---
+++ Controllers/Log-RegController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CAR_GO.Controllers
+{
+    public class Log_RegController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
