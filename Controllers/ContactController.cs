448bf8b30128dab91ee7c603417c0a38dd414135
dersleepy <mostafa.mosalam04@gmail.com>
2024-03-13 21:04:46 +0200

    Added  pages and css

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CAR_GO.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
