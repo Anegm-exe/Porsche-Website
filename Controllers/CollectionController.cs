c2849a6dabf7b4b9c04aa67f7dd980f9d6e2a993
Ahmed Essam Ahmed Hassan <ahmed.essam@student.giu-uni.de>
2024-03-14 15:59:23 +0200

    Updates

---
+++ Controllers/CollectionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Porsche_Website.Controllers
+{
+    public class CollectionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
