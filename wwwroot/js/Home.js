e1a10e489e6796973ebfe8b7653f0cf476963123
Ahmed Essam Ahmed Hassan <ahmed.essam@student.giu-uni.de>
2024-03-14 19:57:57 +0200

    SideBar Fixed :))

--- wwwroot/js/Home.js
+++ wwwroot/js/Home.js
@@ -2,7 +2,7 @@
     const adBanner = document.querySelector('.home-header');
     const video = document.querySelector('#Homevid');
 
-    if (window.scrollY > adBanner.offsetHeight) { /* Change 'X' value as needed */
+    if (window.scrollY > adBanner.offsetHeight) {
         video.classList.add('fixed-video');
     } else {
         video.classList.remove('fixed-video');
