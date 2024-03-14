40f5f846f5b16397fc95421c4b6ea07922251798
Ahmed Essam Ahmed Hassan <ahmed.essam@student.giu-uni.de>
2024-03-14 19:21:29 +0200

    Latest layout fix!

--- wwwroot/js/Home.js
+++ wwwroot/js/Home.js
@@ -1,1 +1,10 @@
-﻿
+﻿document.addEventListener('scroll', function () {
+    const adBanner = document.querySelector('.home-header');
+    const video = document.querySelector('#Homevid');
+
+    if (window.scrollY > adBanner.offsetHeight) { /* Change 'X' value as needed */
+        video.classList.add('fixed-video');
+    } else {
+        video.classList.remove('fixed-video');
+    }
+});
