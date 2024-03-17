578ef4f7452249a26cc69bcca972b88b511ead93
Ahmed Essam Ahmed Hassan <ahmed.essam@student.giu-uni.de>
2024-03-17 18:01:09 +0200

    Sign

---
+++ Controllers/SignController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Porsche_Website.Controllers
+{
+    public class SignController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
